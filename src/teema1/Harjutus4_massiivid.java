922edc415f27ab07ca74b03d67c214ba2dfa280f
rturi <rolandtyri@gmail.com>
2015-10-25 14:30:49 +0200

    4.1 done

--- src/teema1/Harjutus4_massiivid.java
+++ src/teema1/Harjutus4_massiivid.java
@@ -1,5 +1,8 @@
 package teema1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * 1. Salvesta kasutajalt 10 numbrit ja sorteeri suuruse järgi. (int[])
  * 2. Salvesta kasutajalt 10 sõne ja sorteeri tähestikulises järjekorras. (String[])
@@ -9,4 +12,22 @@ package teema1;
  * main() meetodi saad lühendiga psvm.
  */
 public class Harjutus4_massiivid {
+
+    public static void main(String[] args) {
+
+        int[] inputNumbers = new int[10];
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Sisesta " + (i + 1) + ". number:");
+            inputNumbers[i] = sc.nextInt();
+        }
+
+        Arrays.sort(inputNumbers);
+
+        System.out.println("sisestatud numbrid sorteerituna " + Arrays.toString(inputNumbers));
+
+    }
+
 }
