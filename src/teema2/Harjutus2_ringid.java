6410b4ccce4ecf41e86d55ae9432b25fc2234d97
rturi <rolandtyri@gmail.com>
2015-10-31 17:26:46 +0200

    31.10 tunni loppseis

--- src/teema2/Harjutus2_ringid.java
+++ src/teema2/Harjutus2_ringid.java
@@ -2,7 +2,7 @@ package teema2;
 
 /**
  * 1. Joonista kast kasutades jooni
- * 2. Joonista spiraal kasutades jooni (vaata ringi meetodi)
+ * 2. Joonista spiraal kasutades jooni (vaata ringi meetodi) //ringi pole?
  * 3. Joonista propeller (nagu propeller.png) kasutades jooni
  */
 public class Harjutus2_ringid {
--- src/teema2/Harjutus3_logisisse.java
+++ src/teema2/Harjutus3_logisisse.java
@@ -1,15 +1,18 @@
 package teema2;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
 /**
  * 1. Loo sisse logimise ekraan (ainult visuaal)
@@ -17,5 +20,32 @@ import javafx.stage.Stage;
  * 3. Ebaõnnestunud katse näitab kasutajale errorit
  * 4. Õnnestunud katse puhul vaheta pilt uue vastu (kasvõi roheline ring), .
  */
-public class Harjutus3_logisisse {
+public class Harjutus3_logisisse extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        String parool = "123456";
+        VBox vBox = new VBox();
+        Scene stseen = new Scene(vBox);
+        primaryStage.setScene(stseen);
+        primaryStage.show();
+
+        Label kasutajaSilt = new Label("Kasutajanimi");
+        TextField kasutajaTekst = new TextField();
+        Label parooliSilt = new Label("Parool");
+        PasswordField parooliSisend = new PasswordField();
+        Button nupp = new Button("Sisesta");
+        Label infoSilt = new Label();
+
+
+        nupp.setOnAction(event -> {
+            System.out.println("klik");
+            String kasutajaSisu = kasutajaTekst.getText();
+        });
+
+        vBox.getChildren().addAll(kasutajaSilt, kasutajaTekst, parooliSilt, parooliSisend, nupp, infoSilt);
+
+
+    }
 }
